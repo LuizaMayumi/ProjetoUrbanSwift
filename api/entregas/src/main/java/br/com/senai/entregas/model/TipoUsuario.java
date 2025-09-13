d178b612121664164915df7a0d2a6ea1c8585b5c
FIC <fic@info.local>
2025-09-12 21:43:19 -0300

    Criacao de tabelas de usuario e TipoUsuario

---
+++ api/entregas/src/main/java/br/com/senai/entregas/model/TipoUsuario.java
@@ -0,0 +1,32 @@
+package br.com.senai.entregas.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+// Lombook
+@Getter
+@Setter
+
+//Obrigatorio para o JPA funcionar
+@NoArgsConstructor
+@AllArgsConstructor
+
+
+// JPA
+//Inforoma que a classe, e uma tabela
+@Entity
+// Permite a configuracao da tabela
+@Table(name = "tipo_usuario")
+
+public class TipoUsuario {
+
+//    Define que e chave primaria
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="tipo_usuario_id", nullable=false)
+    private Integer tipoUsuario;
+
+    @Column(name="descricao", nullable=false, columnDefinition = "TEXT")
+    private String descricao;
+
+}
